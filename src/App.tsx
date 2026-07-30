30e2b518fa6ef4982080ad4f0e608fe4c394d8ec
artyomdev250 <artyom.dev250@gmail.com>
2026-07-30 14:17:16 +0500

    hero

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Profile from './components/Profile'
 
 function App() {
   return (
-    <div className="bg-[#f9f9f9] h-screen px-4">
+    <div className="bg-[#f9f9f9] min-h-screen px-4 pb-25">
       <Navbar />
       <Hero />
+      <Profile />
     </div>
   )
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import { LocationAlt } from "@boxicons/react"
+import { UserIdCard } from "@boxicons/react"
+
+function Profile() {
+    return (
+        <div className="mt-25 w-full max-w-225 mx-auto flex gap-3">
+            <div className="max-w-66.25 w-full">
+                <img src="/profile.jpg" alt="profile" className="rounded-2xl w-full h-full object-cover" />
+            </div>
+            <div className="px-6 pt-6 pb-8 border border-[#ededee] bg-white w-full rounded-2xl flex flex-col justify-between gap-5">
+                <div>
+                    <div
+                        className="flex h-13.5 w-13.5 items-center justify-center rounded-xl border border-[#ededee] bg-[#f6f6f6] text-white mb-8"
+                    >
+                        <UserIdCard size="base" color="#77777d" />
+                    </div>
+                    <p className="text-[21px] font-medium">Professional Profile</p>
+                    <p className="text-[#77777d] text-[14px] mt-3 font-medium">With 3+ years of commercial experience in software development, I combine technical expertise with a passion for helping people advance their careers. I create ATS-friendly resumes and career materials that highlight your strengths, communicate your impact, and help you stand out in today's job market. My goal is to help you present your experience with clarity and confidence.</p>
+                </div>
+                <div className="text-[12px] text-[#77777d] flex items-center gap-2">
+                    <LocationAlt color="#77777d" size="xs" />
+                    <p>Based in Ashgabat, Ahal, Turkmenistan</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
